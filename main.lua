7f1acb78a536fc01a1dcf9c9f93115a931705919
lhz-piruz <moraesper23@gmail.com>
2026-06-11 01:53:24 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,6 +13,7 @@ local ALLOWED_USERS = {
     [2646021845] = true, --ap
     [10603802243] = true, --ap
         [10893753514] = true, --ap
+    [8932382120] = true, --ap
 }
 
 if game.PlaceId ~= ALLOWED_PLACE then
